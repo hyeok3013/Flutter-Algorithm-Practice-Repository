0402d685d9d37a7a603e27e81302a81970e8317f
Hayo <80828408+hyeok3013@users.noreply.github.com>
2024-02-22 10:02:51 +0900

    Time: 348 ms (24.71%), Space: 147.5 MB (89.41%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.dart
@@ -0,0 +1,30 @@
+class Solution {
+  List<String> fizzBuzz(int n) {
+
+      List<String> answer = [];
+      for (int i = 1; i < n+1; i++){
+
+      if (i % 3 == 0 && i % 5 == 0) {
+          answer.add('FizzBuzz');
+      }
+      else if (i % 3 == 0) {
+          answer.add('Fizz');
+      }
+      else if (i % 5 == 0) {
+          answer.add('Buzz');
+      }
+      else {
+          answer.add(i.toString());
+          //result.add('$i');
+      }
+
+      }
+
+      
+      
+
+    return answer;
+  }
+    
+
+}
